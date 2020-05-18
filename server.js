11d560b785ee6aeaf06d0faee64ceb1519ecd0ce
Pranav Bodawala <b.pranav.k@gmail.com>
2020-05-18 14:46:18 -0600

    more changes

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/dist"));
-app.listen(PORT);
+app.use(express.static(__dirname + "/dist/mmsg-client"));
 
 // PathLocationStrategy
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/dist/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/mmsg-client/index.html"));
 });
 
+app.listen(PORT);
 console.log("Server Listening!");
