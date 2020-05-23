65d7b385e45899595097e86d5efbbff675502ecc
Pranav Bodawala <b.pranav.k@gmail.com>
2020-05-22 18:18:18 -0600

    still trying to fix viewport thing

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,9 @@
 import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  ElementRef,
+    Component,
+    OnInit,
+    AfterViewInit,
+    ViewChild,
+    ElementRef,
 } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SocketioService } from '../socketio.service';
@@ -13,85 +13,98 @@ import { ChatMessage } from 'src/models/chat-message.model';
 import { MsgType } from 'src/models/msg-type.model';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss'],
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-  @ViewChild('msgsContainer') msgContainer: ElementRef;
+    @ViewChild('msgsContainer') msgContainer: ElementRef;
 
-  public username: string;
-  public room: string;
-  public message: ChatMessage;
-  // public message_others: ChatMessage;
-  // public message_self: ChatMessage;
-  // public message_server: ChatMessage;
-  chatMessages: ChatMessage[] = [];
+    public username: string;
+    public room: string;
+    public message: ChatMessage;
+    // public message_others: ChatMessage;
+    // public message_self: ChatMessage;
+    // public message_server: ChatMessage;
+    chatMessages: ChatMessage[] = [];
 
-  public typedText = "";
+    public typedText = '';
 
-  constructor(
-    private socketService: SocketioService,
-    private route: ActivatedRoute
-  ) {}
+    constructor(
+        private socketService: SocketioService,
+        private route: ActivatedRoute
+    ) {}
 
-  ngOnInit(): void {
-    this.joinRoom();
+    ngOnInit(): void {
+        this.setHeight();
 
-    this.socketService.chatMessages$
-      .pipe(
-        tap((chatMsg: ChatMessage) => this.chatMessages.push(chatMsg)),
-        delay(10),
-        tap(() => this.scrollToBottom())
-      )
-      .subscribe();
+        this.joinRoom();
 
-    // this.message_others = new ChatMessage();
-    // this.message_others.from = "Madeeha";
-    // this.message_others.text = "sacsacasc";
-    // this.message_others.type = MsgType.FROM_OTHERS;
+        this.socketService.chatMessages$
+            .pipe(
+                tap((chatMsg: ChatMessage) => this.chatMessages.push(chatMsg)),
+                delay(10),
+                tap(() => this.scrollToBottom())
+            )
+            .subscribe();
 
-    // this.message_self = new ChatMessage();
-    // this.message_self.from = "Pranav";
-    // this.message_self.text = "sacsacasc";
-    // this.message_self.type = MsgType.FROM_SELF;
+        // this.message_others = new ChatMessage();
+        // this.message_others.from = "Madeeha";
+        // this.message_others.text = "sacsacasc";
+        // this.message_others.type = MsgType.FROM_OTHERS;
 
-    // this.message_server = new ChatMessage();
-    // this.message_server.from = "Server";
-    // this.message_server.text = "sacsacasc";
-    // this.message_server.type = MsgType.SERVER;
-  }
+        // this.message_self = new ChatMessage();
+        // this.message_self.from = "Pranav";
+        // this.message_self.text = "sacsacasc";
+        // this.message_self.type = MsgType.FROM_SELF;
 
-  joinRoom() {
-
-    this.route.queryParams.subscribe((params) => {
-      this.username = params.username;
-      this.room = params.room;
-      this.socketService.joinRoom(this.username, this.room);
-    })
-    
-  }
-
-  onSendMsg() {
-    if (this.typedText != "") {
-      this.socketService.sendMsg(this.typedText);
-      this.typedText = "";
-    }
-    // this.message = '';
-  }
-
-  scrollToBottom() {
-
-    if (this.msgContainer) {
-      let container = this.msgContainer.nativeElement;
-      container.scroll({
-        top: container.scrollHeight,
-        left: 0,
-        behavior: 'smooth',
-      });
+        // this.message_server = new ChatMessage();
+        // this.message_server.from = "Server";
+        // this.message_server.text = "sacsacasc";
+        // this.message_server.type = MsgType.SERVER;
     }
 
-    // let container = document.querySelector('#msg-container');
-    // container.scrollTop = container.scrollHeight;
-  }
+    joinRoom() {
+        this.route.queryParams.subscribe((params) => {
+            this.username = params.username;
+            this.room = params.room;
+            this.socketService.joinRoom(this.username, this.room);
+        });
+    }
+
+    onSendMsg() {
+        if (this.typedText != '') {
+            this.socketService.sendMsg(this.typedText);
+            this.typedText = '';
+        }
+        // this.message = '';
+    }
+
+    scrollToBottom() {
+        if (this.msgContainer) {
+            let container = this.msgContainer.nativeElement;
+            container.scroll({
+                top: container.scrollHeight,
+                left: 0,
+                behavior: 'smooth',
+            });
+        }
+
+        // let container = document.querySelector('#msg-container');
+        // container.scrollTop = container.scrollHeight;
+    }
+
+    setHeight() {
+        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        let vh = window.innerHeight * 0.01;
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+        // We listen to the resize event
+        window.addEventListener('resize', () => {
+            // We execute the same script as before
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
+    }
 }
